455683e9feec9b9cafa4ef96a1b0c57404df1011
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2023-06-03 18:06:48 +0200

    feat(base): main class

---
+++ src/main/kotlin/world/anhgelus/msmp/basicmazeworldgenerator/BasicMazeWorldGenerator.kt
@@ -0,0 +1,17 @@
+package world.anhgelus.msmp.basicmazeworldgenerator
+
+import world.anhgelus.msmp.msmpcore.PluginBase
+
+class BasicMazeWorldGenerator: PluginBase() {
+    override val pluginName = "BasicMazeWorldGenerator"
+
+    override fun disable() {
+
+    }
+
+    override fun enable() {
+
+    }
+
+    companion object: CompanionBase()
+}
