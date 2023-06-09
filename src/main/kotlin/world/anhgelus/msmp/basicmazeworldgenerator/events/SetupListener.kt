229f41e10ce5f6a4bec772b4e9fe6a166ebdb244
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2023-06-09 23:19:35 +0200

    feat(spawn): player spawn at 0 0 (+- 2)

--- src/main/kotlin/world/anhgelus/msmp/basicmazeworldgenerator/events/SetupListener.kt
+++ src/main/kotlin/world/anhgelus/msmp/basicmazeworldgenerator/events/SetupListener.kt
@@ -1,6 +1,9 @@
 package world.anhgelus.msmp.basicmazeworldgenerator.events
 
 import org.bukkit.Bukkit
+import org.bukkit.GameRule
+import org.bukkit.Location
+import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.world.WorldLoadEvent
@@ -12,5 +15,18 @@ class SetupListener: Listener {
     fun onWorldGenerationComplete(event: WorldLoadEvent) {
         if (!Datapack.copyInDir(event.world)) BasicMazeWorldGenerator.LOGGER.info("The datapack was not copied to the world folder. " +
                 "It must be a bug if it's the first time you generate the world.")
+        event.world.setGameRule(GameRule.SPAWN_RADIUS, 0)
+        val loc = if (event.world.getBlockAt(0, 65, 0).type == Material.AIR) {
+            Location(event.world, 0.0, 65.0, 0.0)
+        } else if (event.world.getBlockAt(1, 65, 0).type == Material.AIR) {
+            Location(event.world, 1.0, 65.0, 0.0)
+        } else if (event.world.getBlockAt(0, 65, 1).type == Material.AIR) {
+            Location(event.world, 0.0, 65.0, 1.0)
+        } else if (event.world.getBlockAt(1, 65, 1).type == Material.AIR) {
+            Location(event.world, 1.0, 65.0, 1.0)
+        } else {
+            Location(event.world, 2.0, 65.0, 1.0)
+        }
+        event.world.setSpawnLocation(loc)
     }
 }
