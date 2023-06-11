0b903c04c20274c95bbe8a6f4e2ca366d8c188eb
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2023-06-11 13:31:31 +0200

    feat(win): win timer condition

--- src/main/kotlin/world/anhgelus/msmp/basicmazeworldgenerator/api/WinningHandler.kt
+++ src/main/kotlin/world/anhgelus/msmp/basicmazeworldgenerator/api/WinningHandler.kt
@@ -1,14 +1,21 @@
 package world.anhgelus.msmp.basicmazeworldgenerator.api
 
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.GameMode
+import org.bukkit.Location
 import org.bukkit.entity.Player
 import world.anhgelus.msmp.msmpcore.player.MPlayer
 import world.anhgelus.msmp.msmpcore.player.MPlayerManager
+import world.anhgelus.msmp.msmpcore.utils.ChatHelper
 
 abstract class WinningHandler {
     /**
      * Winners list (Place, Player)
      */
     protected val winners = mutableMapOf<Int, MPlayer>()
+
+    protected lateinit var exitLocation: Location
 
     /**
      * Add a winner
@@ -28,5 +35,21 @@ abstract class WinningHandler {
 
     fun getWinners(): Map<Int, MPlayer> {
         return winners
+    }
+
+    protected open fun end() {
+        ChatHelper.sendSuccess("End of the game!")
+        if (winners.size == 1) {
+            ChatHelper.sendSuccess("The winner is ${winners[0]!!.player.displayName}!")
+           return
+        }
+        ChatHelper.sendSuccess("And the winners are:")
+        winners.forEach {
+            ChatHelper.send("${ChatColor.RESET}${it.key}- ${ChatHelper.success}${it.value.player.displayName}")
+        }
+        Bukkit.getOnlinePlayers().forEach {
+            it.gameMode = GameMode.SPECTATOR
+        }
+        ChatHelper.sendInfo("One of the possible exit was located at x = ${exitLocation.blockX}, z = ${exitLocation.blockZ}")
     }
 }
--- src/main/kotlin/world/anhgelus/msmp/basicmazeworldgenerator/handlers/WinHandler.kt
+++ src/main/kotlin/world/anhgelus/msmp/basicmazeworldgenerator/handlers/WinHandler.kt
@@ -3,5 +3,9 @@ package world.anhgelus.msmp.basicmazeworldgenerator.handlers
 import world.anhgelus.msmp.basicmazeworldgenerator.api.WinningHandler
 
 enum class WinHandler(val id: Int, val handler: WinningHandler) {
-    ONE_WINNER(0, OneWinnerHandler)
+    ONE_WINNER(0, OneWinnerHandler),
+    THIRTY_SECONDS_WINNER(1, TimeWinnerHandler(30)),
+    ONE_MINUTE_WINNER(2, TimeWinnerHandler(60)),
+    TWO_MINUTES_WINNER(3, TimeWinnerHandler(120)),
+    CUSTOM_MINUTES_WINNER(4, TimeWinnerHandler()),
 }
